8210cb5d03d7845fa447a9045bb1fad530a3cfc8
MiranaDun <MiranaDun@users.noreply.github.com>
2024-06-30 22:30:36 +0000

    deploy: 1cec4f0220bf231dfd2ee0f67b2a9a2e8924709d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1300332079"
+    serviceWorkerVersion: "3714177297"
   }
 });
