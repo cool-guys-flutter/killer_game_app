bf544f687f60d27e290669714828edb43e24c3d3
pop-arthur <pop-arthur@users.noreply.github.com>
2024-07-01 20:52:08 +0000

    deploy: 207eb6e55904c643627fbe687d403761847eb7f8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1140027912"
+    serviceWorkerVersion: "3576983516"
   }
 });
